a8ced3983a359b11846c329b83171f398034dceb
mumo-rhoda <rhodamumo117@gmail.com>
2023-06-28 22:32:20 +0300

    script update

---
+++ todos.js
@@ -0,0 +1,16 @@
+[
+    {
+    id: 1,
+    title: "",
+    todo: "Do something nice for someone I care about",
+    completed: true,
+
+  },
+  {
+    id: 1,
+    title:"uu",
+    todo: "Do something nice for someone I care about",
+    completed: true,
+    
+    },
+]
